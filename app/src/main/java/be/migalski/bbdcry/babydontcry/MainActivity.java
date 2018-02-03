97e210b1700555df984d8793879602d6301d6c31
RobinMglsk <robin@migalski.be>
2018-02-03 10:58:11 +0100

    Initial commit

---
+++ app/src/main/java/be/migalski/bbdcry/babydontcry/MainActivity.java
@@ -0,0 +1,21 @@
+package be.migalski.bbdcry.babydontcry;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startQuiz(View view){
+        Intent i = new Intent(this, theQuize.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/be/migalski/bbdcry/babydontcry/theQuize.java
@@ -0,0 +1,14 @@
+package be.migalski.bbdcry.babydontcry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class theQuize extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_quize);
+    }
+}
